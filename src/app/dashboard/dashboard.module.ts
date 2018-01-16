b10e988011fa06b5492c1caf5a7af632868d98e6
AChiang <achiang@ftftfund.com.tw>
2018-01-16 16:46:38 +0800

    adjust main component layout

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatTableModule, MatPaginatorModule, MatSortModule } from '@angular/material';
+import { SharedModule } from '../shared/shared.module';
 
 //import { DashboardRoutingModule } from './dashboard-routing.module';
 import { MainComponent } from './main/main.component';
@@ -8,9 +8,7 @@ import { MainComponent } from './main/main.component';
 @NgModule({
   imports: [
     CommonModule,
-    MatTableModule,
-    MatPaginatorModule,
-    MatSortModule/*
+    SharedModule/*
     DashboardRoutingModule*/
   ],
   declarations: [MainComponent]
