2d35731f7db33e00804f2c2c3d56aa6323846935
AChiang <achiang@ftftfund.com.tw>
2018-01-16 14:02:11 +0800

    webanalytics layout ready

--- src/app/web-analytics/web-analytics.component.ts
+++ src/app/web-analytics/web-analytics.component.ts
@@ -9,22 +9,55 @@ import * as moment from 'moment';
   styleUrls: ['./web-analytics.component.css']
 })
 export class WebAnalyticsComponent implements OnInit {
+  analyticsTypeList: any[];
+  today = moment();
   analyticsForm: FormGroup;
 
   constructor() {
-    this.analyticsForm = new FormGroup({
-      analyticsDate: new FormControl('', Validators.required),
-      browseVolume: new FormControl('', Validators.required),
-      visitCount: new FormControl('', Validators.required),
-      userCount: new FormControl('', Validators.required),
-      avgTime: new FormControl('', Validators.required),
-      stayTime: new FormControl('', Validators.required),
-      pageCount: new FormControl('', Validators.required),
-      exitRate: new FormControl('', Validators.required)
-    });
+    this.createForm();
   }
 
   ngOnInit() {
+    this.analyticsTypeList = [{
+      id: 1,
+      name: '全部'
+    },
+    {
+      id: 2,
+      name: 'ETRADE'
+    },
+    {
+      id: 3,
+      name: '手機官網'
+    },
+    {
+      id: 4,
+      name: '桌機官網'
+    }];
   }
 
+  private createForm() {
+    this.analyticsForm = new FormGroup({
+      analyticsType: new FormControl(1, Validators.required),
+      analyticsDate: new FormControl(this.today, Validators.required),
+      browseVolume: new FormControl(0, Validators.required),
+      visitCount: new FormControl(0, Validators.required),
+      userCount: new FormControl(0, Validators.required),
+      avgTime: new FormControl(0, Validators.required),
+      stayTime: new FormControl(0, Validators.required),
+      pageCount: new FormControl(0, Validators.required),
+      exitRate: new FormControl(0.00, Validators.required)
+    });
+  }
+
+  public saveForm() {
+    if (this.analyticsForm.valid) {
+      console.log(this.analyticsForm.value);
+      this.createForm();
+    }
+  }
+
+  public clearForm() {
+    this.createForm();
+  }
 }
