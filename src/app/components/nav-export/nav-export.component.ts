8676c7d13468a6b7f285972808ece41f90fe5074
Aaron <aaron.chiang.tw@gmail.com>
2018-01-18 08:49:50 +0800

    update navexport layout

--- src/app/components/nav-export/nav-export.component.ts
+++ src/app/components/nav-export/nav-export.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, ValidatorFn, Validators, FormGroupDirective, NgForm } from '@angular/forms';
+import { ErrorStateMatcher, MatDatepickerInputEvent } from '@angular/material';
+import { Http } from '@angular/http';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-nav-export',
@@ -6,10 +10,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-export.component.css']
 })
 export class NavExportComponent implements OnInit {
+  exportForm: FormGroup;
+  minDate = moment().add(-1, 'months');
+  maxDate = moment().add(-1, 'days');
 
-  constructor() { }
+  constructor(private _http: Http) {
+    this.createForm();
+  }
 
   ngOnInit() {
   }
 
+  private createForm() {
+    this.exportForm = new FormGroup({
+      ExportDate: new FormControl(this.maxDate, Validators.required)
+    });
+  }
 }
